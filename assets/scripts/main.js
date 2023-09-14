766c97a56557fa438a3786cba1382b7830dfab72
Vlad <vladyslav.pohorilyi@gmail.com>
2023-09-14 10:38:03 +0200

    last change

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -46,3 +46,26 @@ if(item.href === url ){
 }   
 })
 
+// dropdown mobile menu 
+
+let dropdownBtn = document.querySelectorAll('.mobile__item.dropdown');
+
+
+dropdownBtn.forEach(el=>{
+    el.addEventListener('click',()=>{
+        el.classList.toggle('active')
+        if([...dropdownBtn].find(item=> {
+            if(item !== el){
+                item.classList.remove('active')
+                el.classList.toggle('active')
+            }else{
+                el.classList.toggle('active')
+            }
+            
+        })){
+            
+        }
+
+    })
+
+})
